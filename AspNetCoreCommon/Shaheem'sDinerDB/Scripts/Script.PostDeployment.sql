a1c2b87d21ecb88b0b260dda81ceb6eb867d8920
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-09-08 12:29:42 +0530

    Added a Post script file to put data into food tbl

---
+++ AspNetCoreCommon/Shaheem'sDinerDB/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+if not exists (select 1 from Food)
+begin
+insert into Food(Title,Description,Price) 
+values('Cheesburger Meal', 'A cheeseburger, fries, and a drink' , 300 ),
+('Eid Meal', 'A Beef biriyani and Semiya payasam' , 250),
+('Marriage Meal', 'Chicken Mandi with alfaham chicken',500)
+end
