3bb45334f80ffd7914f7adb419244048757f3294
muhammed shaheem <ppmohd038@gmail.com>
2025-09-08 12:17:11 +0530

    Added DB structure for the project.

---
+++ AspNetCoreCommon/Shaheem'sDinerDB/Food.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Food]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(250) NOT NULL, 
+    [Price ] MONEY NOT NULL
+)
