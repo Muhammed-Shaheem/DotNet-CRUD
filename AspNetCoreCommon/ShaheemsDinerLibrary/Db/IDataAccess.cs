11f318a41696c9dc194398f783c3f90a7fb3b18c
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-09-09 13:44:41 +0530

    Added Sql Database Access methods .

---
+++ AspNetCoreCommon/ShaheemsDinerLibrary/Db/IDataAccess.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ShaheemsDinerLibrary.Db
+{
+    public interface IDataAccess
+    {
+        List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName, bool isStoreProcedure);
+        void SaveData<T>(string StoredProcedure, T parameters, string connectionStringName);
+    }
+}
---
+++ AspNetCoreCommon/ShaheemsDinerLibrary/Db/SqlDb.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace ShaheemsDinerLibrary.Db;
+
+
+public class SqlDb : IDataAccess
+{
+    private readonly IConfiguration config;
+
+    public SqlDb(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName, bool isStoreProcedure)
+    {
+        var connectionString = config.GetConnectionString(connectionStringName);
+
+        using (IDbConnection db = new SqlConnection(connectionString))
+        {
+            return db.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure).ToList();
+        }
+    }
+
+    public void SaveData<T>(string StoredProcedure, T parameters, string connectionStringName)
+    {
+        var connectionString = config.GetConnectionString(connectionStringName);
+        using (IDbConnection db = new SqlConnection(connectionString))
+        {
+            db.Execute(StoredProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
