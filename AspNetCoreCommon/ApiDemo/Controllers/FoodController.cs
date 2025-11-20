cbdf200acae8921572faf3cc5d68edc698967617
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-11-20 10:34:08 +0530

    Added a Get method for food controller

---
+++ AspNetCoreCommon/ApiDemo/Controllers/FoodController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShaheemsDinerLibrary.Data;
+using System.Threading.Tasks;
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly IFoodData foodData;
+
+        public FoodController(IFoodData foodData)
+        {
+            this.foodData = foodData;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+           var food = await foodData.GetFood();
+
+            return Ok(food);
+            
+        }
+    }
+}
