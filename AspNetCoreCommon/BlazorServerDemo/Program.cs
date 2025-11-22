d4c71d228eb3ad669726d571fe88c61a1595483b
Muhammed Shaheem <Shaheemofcl@gmail.com>
2025-11-22 09:59:02 +0530

    Added dependency injection setup to the project

--- AspNetCoreCommon/BlazorServerDemo/Program.cs
+++ AspNetCoreCommon/BlazorServerDemo/Program.cs
@@ -1,21 +1,27 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorServerDemo.Data;
+using ShaheemsDinerLibrary.Data;
+using ShaheemsDinerLibrary.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddTransient<IDataAccess, SqlDb>();
+builder.Services.AddTransient<IFoodData, FoodData>();
+builder.Services.AddTransient<IOrderData, OrderData>();
+builder.Services.AddSingleton(new ConnectionStringName
+{
+    SqlConnection = "Default"
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
